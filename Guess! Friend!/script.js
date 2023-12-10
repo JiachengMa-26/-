b8779bb2bac41b362dad8fe1b4743f394020a5b9
Jiacheng Ma-26 <666mjc666@gmail.com>
2023-12-09 22:41:44 -0500

    sm update

--- Guess! Friend!/script.js
+++ Guess! Friend!/script.js
@@ -77,6 +77,11 @@
             options: {A: "三井寿", B: "皇家程序员", C: "妮子", D: "我不知道啊！"},
             answer: {A: "松洺", B: "小生命", C: "娅妮", D: null}
         },
+        {
+            question: "什么是一个“典中典”？",
+            options: {A: "锤石预判Q", B: "上课完Minecraft", C: "All Gender", D: "我不知道啊！"},
+            answer: {A: "松洺", B: "小生命", C: "李猛", D: null}
+        },
     ];
 
 
